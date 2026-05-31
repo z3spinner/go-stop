39e0b876b1b3f56f50adb9bbacff5e49b927dffb
Zeno Kerr <zeno@calmblue.co.uk>
2026-05-31 19:46:46 +0200

    feat: wire up application in main.go

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/z3spinner/go-stop/internal/boundaries/handler"
+	"github.com/z3spinner/go-stop/internal/infrastructure/postgres"
+	"github.com/z3spinner/go-stop/internal/infrastructure/webpush"
+	"github.com/z3spinner/go-stop/internal/usecase"
+)
+
+func main() {
+	pool, err := postgres.NewPool()
+	if err != nil {
+		log.Fatalf("database: %v", err)
+	}
+	defer pool.Close()
+
+	rideRepo := postgres.NewRideRepo(pool)
+	requestRepo := postgres.NewRequestRepo(pool)
+	destRepo := postgres.NewDestinationRepo(pool)
+	subRepo := postgres.NewSubscriptionRepo(pool)
+
+	notifier := webpush.New(
+		os.Getenv("VAPID_PUBLIC_KEY"),
+		os.Getenv("VAPID_PRIVATE_KEY"),
+		os.Getenv("VAPID_EMAIL"),
+	)
+
+	postRide := usecase.NewPostRide(rideRepo, requestRepo, subRepo, notifier)
+	postRequest := usecase.NewPostRequest(requestRepo, rideRepo, subRepo, notifier)
+	getRides := usecase.NewGetRides(rideRepo)
+	searchRides := usecase.NewSearchRides(rideRepo)
+	getDests := usecase.NewGetDestinations(destRepo)
+	subscribe := usecase.NewSubscribe(subRepo)
+	unsubscribe := usecase.NewUnsubscribe(subRepo)
+	deleteRide := usecase.NewDeleteRide(rideRepo)
+	deleteRequest := usecase.NewDeleteRequest(requestRepo)
+	expireRides := usecase.NewExpireRides(rideRepo)
+	expireRequests := usecase.NewExpireRequests(requestRepo)
+
+	rideH := handler.NewRideHandler(postRide, getRides, searchRides, deleteRide, rideRepo)
+	requestH := handler.NewRequestHandler(postRequest, deleteRequest, requestRepo)
+	destH := handler.NewDestinationHandler(getDests)
+	subH := handler.NewSubscriptionHandler(subscribe, unsubscribe)
+
+	go func() {
+		ticker := time.NewTicker(time.Hour)
+		defer ticker.Stop()
+		for range ticker.C {
+			if err := expireRides.Execute(); err != nil {
+				log.Printf("expire rides: %v", err)
+			}
+			if err := expireRequests.Execute(); err != nil {
+				log.Printf("expire requests: %v", err)
+			}
+		}
+	}()
+
+	r := gin.Default()
+	r.Static("/css", "./web/css")
+	r.Static("/js", "./web/js")
+	r.StaticFile("/manifest.json", "./web/manifest.json")
+	r.StaticFile("/sw.js", "./web/js/sw.js")
+	r.NoRoute(func(c *gin.Context) {
+		c.File("./web/index.html")
+	})
+
+	api := r.Group("/api")
+	{
+		api.POST("/rides", rideH.Post)
+		api.GET("/rides", rideH.List)
+		api.GET("/rides/:id", rideH.Get)
+		api.DELETE("/rides/:id", rideH.Delete)
+
+		api.POST("/requests", requestH.Post)
+		api.GET("/requests/:id", requestH.Get)
+		api.DELETE("/requests/:id", requestH.Delete)
+
+		api.GET("/destinations", destH.List)
+
+		api.POST("/subscriptions", subH.Subscribe)
+		api.DELETE("/subscriptions/:phone", subH.Unsubscribe)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("listening on :%s", port)
+	if err := r.Run(":" + port); err != nil {
+		log.Fatalf("server: %v", err)
+	}
+}
