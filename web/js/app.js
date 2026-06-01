5242cc40283d93fbac16a7e0c1ddbc8a23118dff
Zeno Kerr <zeno@calmblue.co.uk>
2026-06-01 11:13:12 +0200

    fix: SVG outline bell icon, moved right of ⓘ, amber/green stroke colours

--- web/js/app.js
+++ web/js/app.js
@@ -970,7 +970,7 @@ async function handleInterestClick(btn) {
 }
 
 function bellIcon() {
-  return `<button class="btn-bell" id="btn-bell" aria-label="Notifications">🔔</button>`;
+  return `<button class="btn-bell" id="btn-bell" aria-label="Notifications"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15" fill="none" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 0 1-3.46 0"/></svg></button>`;
 }
 
 async function updateBellState() {
@@ -1020,7 +1020,7 @@ async function handleBellClick() {
 }
 
 function controls() {
-  return `<div class="controls">${langToggle()}${bellIcon()}${aboutIcon()}</div>`;
+  return `<div class="controls">${langToggle()}${aboutIcon()}${bellIcon()}</div>`;
 }
 
 function topBar() {
